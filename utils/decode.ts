c450a945ec9c365883f8be1f30cc6858badef00c
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-05-07 16:36:39 +0500

    Add console error on decode error

--- utils/decode.ts
+++ utils/decode.ts
@@ -4,6 +4,10 @@ const decode = <T>(body: unknown, decoder: JsonDecoder.Decoder<T>): T => {
   const result = decoder.decode(body)
   
   if (!result.isOk()) {
+    console.error(`
+Can't decode this object:
+${JSON.stringify(body)}
+`)
     throw new Error(result.error)
   }
   
