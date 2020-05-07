2bd7cb11640bba37d556c74f4ff05e31d3c33e6a
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-05-07 14:02:55 +0500

    Remove circular imports (?)

--- utils/decode.ts
+++ utils/decode.ts
@@ -1,11 +1,9 @@
 import { JsonDecoder } from 'ts.data.json'
-import variables from './variables'
 
 const decode = <T>(body: unknown, decoder: JsonDecoder.Decoder<T>): T => {
   const result = decoder.decode(body)
   
   if (!result.isOk()) {
-    console.log(JSON.stringify(variables))
     throw new Error(result.error)
   }
   
