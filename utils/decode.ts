754ab62183dc2cbd41c0a1b64f82cb7e7434ea80
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-05-07 08:14:22 +0500

    Add console.log variables

--- utils/decode.ts
+++ utils/decode.ts
@@ -1,9 +1,11 @@
 import { JsonDecoder } from 'ts.data.json'
+import variables from './variables'
 
 const decode = <T>(body: unknown, decoder: JsonDecoder.Decoder<T>): T => {
   const result = decoder.decode(body)
   
   if (!result.isOk()) {
+    console.log(JSON.stringify(variables))
     throw new Error(result.error)
   }
   
