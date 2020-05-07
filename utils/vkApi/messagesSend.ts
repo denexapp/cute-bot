d3ec5bf53990c4335ed39214e3476924c1e65ae8
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-05-07 16:31:06 +0500

    Add message validation

--- utils/vkApi/messagesSend.ts
+++ utils/vkApi/messagesSend.ts
@@ -1,6 +1,8 @@
 import variables from '../variables'
 import consts from '../consts'
 import fetch from 'node-fetch'
+import { JsonDecoder } from 'ts.data.json'
+import decode from '../decode'
 
 type Params = { [key: string]: string | number }
 
@@ -14,6 +16,14 @@ const generateVkLink = (methodName: string, params?: Params): string => {
   return `https://api.vk.com/method/${methodName}?${query}`
 }
 
+interface MessageSend {
+  response: number
+}
+
+const messageSendResponseValidator = JsonDecoder.object<MessageSend>({
+  response: JsonDecoder.number
+}, 'Message send response')
+
 const messagesSend = async (peerId: number, message: string) => {
   const response = await fetch(generateVkLink('messages.send', {
     message,
@@ -23,6 +33,10 @@ const messagesSend = async (peerId: number, message: string) => {
   if (!response.ok) {
     throw new Error(`Unexpected response status ${response.status}`)
   }
+
+  const json = await response.json()
+
+  return decode(json, messageSendResponseValidator)
 }
 
 export default messagesSend
