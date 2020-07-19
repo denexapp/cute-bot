5f08aa0c6a93fd1dcafcd4e3426e23d15778d32a
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-07-20 00:12:21 +0500

    Fix typo

--- utils/messages.ts
+++ utils/messages.ts
@@ -3,7 +3,7 @@ const messages = {
   echoDescription: 'echoes all new messages in the chat',
   echoEnabled: 'Echo enabled',
   echoDisabled: 'Echo disabled',
-  helpDescription: 'shows list of all commands',
+  helpDescription: 'lists all commands',
 }
 
 export default messages
