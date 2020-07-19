c2cf9d308ed7f134dd84044f02325f17357f335d
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-07-20 00:11:09 +0500

    Fix a typo

--- utils/messages.ts
+++ utils/messages.ts
@@ -1,6 +1,6 @@
 const messages = {
   unknownCommand: 'Unknown command. Use /help to list all commands',
-  echoDescription: 'echoes all messages in chat',
+  echoDescription: 'echoes all new messages in the chat',
   echoEnabled: 'Echo enabled',
   echoDisabled: 'Echo disabled',
   helpDescription: 'shows list of all commands',
