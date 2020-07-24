3651d23076277d5d891c2ff5b33305a55c6ed074
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-07-25 01:49:50 +0500

    Fix message mistake

--- utils/messages.ts
+++ utils/messages.ts
@@ -1,7 +1,7 @@
 const messages = {
   unknownCommand: 'Unknown command. Use /help to list all commands',
   commandCalledByLeftUser: 'User who called this command left the chat',
-  commandAvailableForAdminsOnly: 'This command available for admins only',
+  commandAvailableForAdminsOnly: 'This command is available for admins only',
   echoDescription: 'echoes all new messages in the chat',
   echoEnabled: 'Echo enabled',
   echoDisabled: 'Echo disabled',
