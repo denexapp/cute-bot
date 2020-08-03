cdc23c0084e7e4378a70e70b39329c7800e0cc36
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-08-04 01:42:43 +0500

    Add console.log

--- utils/callbackServer/utils/makeRequestToCallback.ts
+++ utils/callbackServer/utils/makeRequestToCallback.ts
@@ -14,6 +14,8 @@ const makeRequestToCallback = async <T>(
     object
   }
 
+  console.log(JSON.stringify(body))
+
   const result = await fetch(url, {
     body: JSON.stringify(body),
     method: 'POST',
