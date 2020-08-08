b378c4d40ca028c2088193217eb59d691360b728
Denis Mukhametov <denis.mukhametov@gmail.com>
2020-08-08 12:20:11 +0500

    Don't use modes and settings in private messages

--- api/vk-callback.ts
+++ api/vk-callback.ts
@@ -1,10 +1,7 @@
 import { NowRequest, NowResponse } from '@now/node'
 import createSettingsCollections from '../utils/database/createSettingsCollections'
-import getChatSettings from '../utils/database/getChatSettings'
 import decodeVkCallback from '../utils/decodeVkCallback'
-import getCallbackServerSettings from '../utils/getCallbackServerSettings'
-import handleCommand from '../utils/handleCommand'
-import handleModes from '../utils/handleModes'
+import handleMessage from '../utils/handleMessage'
 import variables from '../utils/variables'
 
 export default async (req: NowRequest, res: NowResponse) => {
@@ -17,18 +14,9 @@ export default async (req: NowRequest, res: NowResponse) => {
 
   if (data.type === 'message_new') {
     const { message } = data.object
-    const { text, peer_id: peerId } = message
 
     await createSettingsCollections()
-
-    const settings = await getChatSettings(peerId)
-    const callbackServerSettings = await getCallbackServerSettings(settings)
-
-    if (text.startsWith('/')) {
-      await handleCommand(message, settings, callbackServerSettings)
-    }
-
-    await handleModes(message, settings, callbackServerSettings)
+    await handleMessage(message)
 
     res.send('ok')
     return
---
+++ utils/handleMessage.ts
@@ -0,0 +1,32 @@
+import getChatSettings from './database/getChatSettings'
+import getCallbackServerSettings from './getCallbackServerSettings'
+import handleConversationMessage from './handleConversationMessage'
+import handleModes from './handleModes'
+import handlePrivateMessage from './handlePrivateMessage'
+import { Message } from './vkCallbackDecoders/messageNewDecoder'
+
+const getCommandName = (text: string) => {
+  const spaceIndex = text.indexOf(' ')
+  return spaceIndex === -1 ? text : text.slice(0, spaceIndex).slice(1)
+}
+
+const handleMessage = async (message: Message) => {
+  const { text, peer_id: peerId } = message
+  const privateMessage = message.peer_id < 2000000000
+  const command = text.startsWith('/')
+
+  if (!privateMessage) {
+    const settings = await getChatSettings(peerId)
+    const callbackServerSettings = await getCallbackServerSettings(settings)
+    if (command) {
+      await handleConversationMessage(message, getCommandName(text), settings, callbackServerSettings)
+    }
+    await handleModes(message, settings, callbackServerSettings)
+  } else {
+    if (command) {
+      await handlePrivateMessage(message, getCommandName(text))
+    }
+  }
+}
+
+export default handleMessage
