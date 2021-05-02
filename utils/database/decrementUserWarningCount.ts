c123c83b4d6d427993911e772618518ba8c10b26
Denis Mukhametov <denis.mukhametov@gmail.com>
2021-05-02 08:52:55 +0500

    Fix decoder, nullify warning count if its equal zero

--- utils/database/decrementUserWarningCount.ts
+++ utils/database/decrementUserWarningCount.ts
@@ -1,8 +1,9 @@
-import { query as q } from "faunadb"
-import { JsonDecoder } from 'ts.data.json'
-import decode from '../decode'
-import chatSettingsDecoder from "./utils/chatSettingsDecoder"
-import getDatabaseClient from "./utils/getDatabaseClient"
+import { query as q } from "faunadb";
+import { JsonDecoder } from "ts.data.json";
+import decode from "../decode";
+import chatSettingsDecoder from "./utils/chatSettingsDecoder";
+import { databaseResponseDecoder } from "./utils/decodeDatabaseResponse";
+import getDatabaseClient from "./utils/getDatabaseClient";
 
 const decrementUserWarningCount = async (
   peerId: number,
@@ -20,21 +21,34 @@ const decrementUserWarningCount = async (
         ),
       },
       q.If(
-        q.LTE(q.Var("count"), 0),
-        null,
+        q.GT(q.Var("count"), 1),
         q.Update(q.Ref(q.Collection("chats-settings"), peerId), {
           data: {
             warnings: {
               [userId]: q.Subtract(q.Var("count"), 1),
             },
           },
-        })
+        }),
+        q.If(
+          q.Equals(q.Var("count"), 1),
+          q.Update(q.Ref(q.Collection("chats-settings"), peerId), {
+            data: {
+              warnings: {
+                [userId]: null,
+              },
+            },
+          }),
+          null
+        )
       )
     )
   );
 
-  const decodedSettings = decode(settings, JsonDecoder.nullable(chatSettingsDecoder));
-  const userWarningCount = decodedSettings?.warnings[userId] ?? null;
+  const decodedSettings = decode(
+    settings,
+    JsonDecoder.nullable(databaseResponseDecoder(chatSettingsDecoder))
+  );
+  const userWarningCount = decodedSettings?.data.warnings[userId] ?? null;
 
   return userWarningCount;
 };
