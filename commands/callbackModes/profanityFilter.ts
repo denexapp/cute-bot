d986fa92dd169b7a565cb4200a20c2e38dd64f30
Denis Mukhametov <denis.mukhametov@gmail.com>
2021-05-18 21:24:52 +0500

    Do not remove admin messages with swear words

--- commands/callbackModes/profanityFilter.ts
+++ commands/callbackModes/profanityFilter.ts
@@ -1,22 +1,35 @@
 import { CallbackMode, CallbackModeObject } from "..";
 import remove from "../../utils/callbackServer/remove";
 import { flatWords } from "russian-bad-words";
-import incrementUserWarningCount from '../../utils/database/incrementUserWarningCount'
-import { Sex } from 'vk-ts'
-import generateVkUserLink from '../../utils/generateVkUserLink'
-import kick, { Status } from '../../utils/callbackServer/kick'
-import removeUserWarningCount from '../../utils/database/removeUserWarningCount'
-import phrase from '../../utils/localization/phrase'
-import vk from '../../utils/vk'
+import incrementUserWarningCount from "../../utils/database/incrementUserWarningCount";
+import { Sex } from "vk-ts";
+import generateVkUserLink from "../../utils/generateVkUserLink";
+import kick, { Status } from "../../utils/callbackServer/kick";
+import removeUserWarningCount from "../../utils/database/removeUserWarningCount";
+import phrase from "../../utils/localization/phrase";
+import vk from "../../utils/vk";
 
 const action: CallbackMode = async (
   message,
   settings,
-  { callbackSecret, callbackServerChatId, callbackServerUrl, callbackServerUserId },
+  {
+    callbackSecret,
+    callbackServerChatId,
+    callbackServerUrl,
+    callbackServerUserId,
+  },
   botReacted,
   adminContext
 ) => {
-  const { conversation_message_id: conversationMessageId, peer_id: peerId, from_id: fromId } = message;
+  const {
+    conversation_message_id: conversationMessageId,
+    peer_id: peerId,
+    from_id: fromId,
+  } = message;
+
+  if (adminContext?.isAdminMessage) {
+    return;
+  }
 
   const normalizeWord = (word: string) =>
     word.replace("ё", "е").replace("й", "и").replace("ъ", "ь");
@@ -31,7 +44,7 @@ const action: CallbackMode = async (
 
   if (words === null) return;
 
-  let swearWord: string | undefined
+  let swearWord: string | undefined;
 
   for (const word of words) {
     if (swearWordsSet.has(word)) {
@@ -42,17 +55,13 @@ const action: CallbackMode = async (
         conversationMessageId
       );
 
-      swearWord = word
+      swearWord = word;
 
       break;
     }
   }
-  
-  if (swearWord === undefined) return
-  
-  if (adminContext?.isAdminMessage) {
-    return
-  }
+
+  if (swearWord === undefined) return;
 
   const user = adminContext?.profiles.get(fromId);
   const count = await incrementUserWarningCount(peerId, fromId);
@@ -71,7 +80,7 @@ const action: CallbackMode = async (
       phrase("profanityFilter_willBeKickedWithCallback", {
         userLink,
         maxCount,
-        word: swearWord
+        word: swearWord,
       })
     );
 
@@ -88,34 +97,33 @@ const action: CallbackMode = async (
       ).status;
     } catch {}
 
-      if (result === null) {
-          const callbackOwnerProfile = adminContext?.profiles.get(
-            callbackServerUserId
-          );
-          const callbackOwnerLink = generateVkUserLink(
-            callbackServerUserId,
-            callbackOwnerProfile?.first_name
-          );
-          const callbackOwnerSex =
-            callbackOwnerProfile?.sex === Sex.Female
-              ? "female"
-              : callbackOwnerProfile?.sex === Sex.Male
-              ? "male"
-              : "unknown";
-          await vk.messagesSend(
-            peerId,
-            phrase("common_kickWithCallbackServerFailed", {
-              userLink,
-              sex,
-              callbackOwnerLink,
-              callbackOwnerSex,
-            })
-          );
-        }
+    if (result === null) {
+      const callbackOwnerProfile =
+        adminContext?.profiles.get(callbackServerUserId);
+      const callbackOwnerLink = generateVkUserLink(
+        callbackServerUserId,
+        callbackOwnerProfile?.first_name
+      );
+      const callbackOwnerSex =
+        callbackOwnerProfile?.sex === Sex.Female
+          ? "female"
+          : callbackOwnerProfile?.sex === Sex.Male
+          ? "male"
+          : "unknown";
+      await vk.messagesSend(
+        peerId,
+        phrase("common_kickWithCallbackServerFailed", {
+          userLink,
+          sex,
+          callbackOwnerLink,
+          callbackOwnerSex,
+        })
+      );
+    }
 
-      if (result === Status.Kicked) {
-        await removeUserWarningCount(peerId, fromId);
-      }
+    if (result === Status.Kicked) {
+      await removeUserWarningCount(peerId, fromId);
+    }
   } else {
     await vk.messagesSend(
       peerId,
@@ -123,7 +131,7 @@ const action: CallbackMode = async (
         userLink,
         count,
         maxCount,
-        word: swearWord
+        word: swearWord,
       })
     );
   }
